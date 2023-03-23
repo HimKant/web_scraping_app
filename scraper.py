8413acb2e08f0787a55db467cfbed912eb2de5f2
Himkant Nigam <88400217+HimKant@users.noreply.github.com>
2023-03-23 13:24:31 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,2 +1,37 @@
+import pickle
 import streamlit as st
-st.write("Hi! everyone")
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+
+#google api modules
+from googleapiclient.discovery import build
+
+#credentials
+api_key = "AIzaSyBh9_pQA2Vyh2YMgNUBYMWppFaJlR-Qyxs"
+search_engine_id = "403255ea2525b46b4"
+
+
+resource = build("customsearch", "v1" , developerKey = api_key).cse()
+
+#request creation
+
+request = resource.list(q = input("Enter search term :") , cx = search_engine_id)
+
+#request execution
+result = request.execute()
+
+print("\n------------Search--------------\n\n")
+
+print(f"search results (", result["searchInformation"]["searchTime"], ")")
+
+print ("total" , len (result['items']), "results fetched")
+
+for item in result['items']:
+    print("___________________________________________")
+    print("Title:", item["title"]) #title
+    print("Link:", item["link"])
+    print("Description:", item["snippet"])
+    print("___________________________________________\n")
+
